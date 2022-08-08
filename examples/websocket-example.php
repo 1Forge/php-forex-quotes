39a523fdbf7dafdbe76ca374f00ba61ac0e344ec
Aditi <52167793+Aditi-mi@users.noreply.github.com>
2022-08-08 10:38:29 -0400

    wrench package updated and example path changed

--- examples/websocket-example.php
+++ examples/websocket-example.php
@@ -5,7 +5,7 @@
     * Created by 1Forge <contact@1forge.com>
     */
 
-    require_once __DIR__ . './vendor/autoload.php';
+    require_once __DIR__.'/vendor/autoload.php';
 
     use OneForge\ForexQuotes\ForexDataClient;
 
